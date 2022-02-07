6e469942275c5ad6b1981d286affbb017299a4c2
regadior <regadior@gmail.com>
2022-02-07 22:57:35 +0100

    Metodo de areas

---
+++ Metodos_practica/src/metodo_area.java
@@ -0,0 +1,21 @@
+
+/*Crea una aplicación que nos calcule el área de un circulo, cuadrado o triangulo. 
+Pediremos que figura queremos calcular su área y según lo introducido pedirá los valores necesarios para calcular el área. 
+Crea un método por cada figura para calcular cada área, este devolverá un número real. 
+Muestra el resultado por pantalla
+
+Aquí te mostramos que necesita cada figura:
+
+Circulo: (radio^2)*PI
+Triangulo: (base * altura) / 2
+Cuadrado: lado * lado*/
+
+public class metodo_area {
+public static void a_circulo() {
+    
+}
+    public static void main(String[] args) {
+        
+
+    }
+}
