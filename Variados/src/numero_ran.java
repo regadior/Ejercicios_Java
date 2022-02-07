646578b0765666e9d138eba306555fbef0d8de83
regadior <regadior@gmail.com>
2022-02-07 17:45:51 +0100

    Hacer repetir en casa

---
+++ Variados/src/numero_ran.java
@@ -0,0 +1,37 @@
+
+//Crea un programa que genero numeros randon en un intervalo definido por el usuario y jugar a encontrarlo preguntar para repetir y contar aciertos
+
+import java.util.Scanner;
+public class numero_ran {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int contador=0;
+        String seguir="S";
+        while(seguir.equals("S")){
+            seguir="N";
+            System.out.println("Introduce el limite inferior");
+            int inf = sc.nextInt();
+            System.out.println("Introduce el limite superior");
+                int sup = sc.nextInt();
+            if(inf>=sup){
+                sup=0;
+                System.out.println("Introduce el limite superior");
+                sup = sc.nextInt();
+            }
+            int rand = (int) (Math.random()*(inf+sup));
+            System.out.print("Teclea un numero para intentar encontrarlo: ");
+            int num = sc.nextInt();
+            do{
+                System.out.print("Teclea un numero para intentar encontrarlo: ");
+                num = sc.nextInt();
+            }while(num!=rand);
+            if (num==rand) {
+                System.out.println("Has encontrado el numero: "+rand);
+                contador++;
+                System.out.println("Llevas "+contador+" aciertos");
+            }
+            System.out.println("Quieres continuar S / N");
+            seguir = sc.nextLine(); 
+        }
+    }
+}
