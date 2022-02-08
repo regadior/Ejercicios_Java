03e6da696bd8757675114d9dd79479c2c55e7fe4
regadior <regadior@gmail.com>
2022-02-08 11:19:26 +0100

    Areas con metodos terminado

--- Metodos_practica/src/metodo_area.java
+++ Metodos_practica/src/metodo_area.java
@@ -7,15 +7,38 @@ Muestra el resultado por pantalla
 Aquí te mostramos que necesita cada figura:
 
 Circulo: (radio^2)*PI
-Triangulo: (base * altura) / 2
-Cuadrado: lado * lado*/
+Triangulo: (base * altura) / 2 */
 
+import java.util.Scanner;
 public class metodo_area {
-public static void area_circulo() {
-    
-}
-    public static void main(String[] args) {
-        
+    public static double areaCirculo (double radio){
+        System.out.println(Math.pow(radio, 2)*Math.PI);
+        return radio;
+    }
 
+    public static double areaTriangulo(double base, double altura) {
+        System.out.println((base*altura)/2);
+        return base;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String eleccion="";
+        System.out.print("Introduce el nombre de la figura a la que le quieres calcular el area:  ");
+        eleccion = sc.nextLine();
+        switch(eleccion){
+            case "circulo":
+            System.out.print("Introduce el radio de el circulo: ");
+            double radio = sc.nextDouble(); 
+            areaCirculo(radio);
+            break;
+            case "triangulo":
+            System.out.print("Introduce la base de el trinagulo: ");
+            double base = sc.nextDouble();
+            System.out.print("Introduce la altura de el trinagulo: ");
+            double altura = sc.nextDouble();
+            areaTriangulo(base, altura);
+            break;
+        }
     }
 }
