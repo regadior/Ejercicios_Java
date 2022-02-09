565e907deaabd75888b0553584e02c5655b1a164
regadior <regadior@gmail.com>
2022-02-09 13:06:27 +0100

    numeros primos

---
+++ Variados/src/numerosprimos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class numerosprimos {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            int raiz=(int)Math.sqrt(i);
+            int contador=0;
+            for (int j=raiz;j>1;j--){
+                if (i%j==0){
+                    contador++;
+                }
+            }
+            if (contador<1){
+                System.out.println(i);
+            }
+        }
+    }
+}
