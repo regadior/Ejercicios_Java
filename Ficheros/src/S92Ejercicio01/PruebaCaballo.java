3100ca1001fb88566281c537d3150333197c4b6a
regadior <regadior@gmail.com>
2022-02-27 20:05:40 +0100

    1ejer obje

---
+++ Ficheros/src/S92Ejercicio01/PruebaCaballo.java
@@ -0,0 +1,17 @@
+package S92Ejercicio01;
+
+public class PruebaCaballo {
+    public static void main(String[] args) {
+        Caballo a = new Caballo("Avra", "marrón moteado", 6, 24);
+        Caballo l = new Caballo("Lykos", "negro", 8, 61);
+        
+        System.out.println("Hola, me llamo " + a.getNombre());
+        a.cabalga();
+        a.relincha();
+        
+        System.out.println("Hola, yo soy " + l.getNombre());
+        l.rumia();
+        l.cabalga();
+
+    }
+}
