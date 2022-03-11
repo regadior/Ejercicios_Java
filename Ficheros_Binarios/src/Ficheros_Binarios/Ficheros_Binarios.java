ba579bcba665bd6cd2ef47105086500151aaa300
regadior <regadior@gmail.com>
2022-03-11 12:32:55 +0100

    Corregido

--- Ficheros_Binarios/src/Ficheros_Binarios/Ficheros_Binarios.java
+++ Ficheros_Binarios/src/Ficheros_Binarios/Ficheros_Binarios.java
@@ -1,143 +1,146 @@
 package Ficheros_Binarios;
-import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
+@SuppressWarnings("resource")
 public class Ficheros_Binarios {
-    static final String ruta="C:\\Java-practica\\Ficheros_Binarios\\src\\Ficheros_Binarios\\";
-    static int menu(){
-        Scanner sc = new Scanner(System.in);
-        int op=0;
-        System.out.println("MENU");
-        System.out.println("1.Crear Fichero");
-        System.out.println("2.Listar Fichero");
-        System.out.println("3.Buscar Numero");
-        System.out.println("4.Buscar Numero y Veces Que Se Repite");
-        System.out.println("5.Fin");
-        do {
-            System.out.print("Teclea una opcion del 1 a 5--> ");
-            op = sc.nextInt();
-        } while (op<1 || op>5);
-        sc.close();
-        return op;
+static final String ruta="E:\\ESCRITORIO\\CLASES\\PRG";
+static  int menu(Scanner sc) {
+int opcion=0;
+do {
+System.out.println("\n\tMENÚ\n");
+System.out.println("1.Crear fichero.");
+System.out.println("2.Listar fichero.");
+System.out.println("3.Buscar número.");
+System.out.println("4.Buscar número y veces que se repite.");
+System.out.println("5.Fin de programa.");
+System.out.print("\n\tTeclee opción(1-5)? ");
+opcion=sc.nextInt();
+}while (opcion>5 || opcion <1);
+return opcion;
 
-    }
+}
+static void crearFichero(){
+System.out.println("\n\tCREAR FICHERO\n");
+Scanner sc=new Scanner(System.in);
+int numero=0;
+try {
+ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream(ruta+"numeros.dat"));
+System.out.println("Teclee números (999 para finalizar):");
+while(numero!=999) {
+numero=sc.nextInt();
+salida.writeInt(numero);
+}
+salida.close();
+}catch(IOException ioe) {
+System.out.println("El fichero no se puede abrir");
+}
+}
 
-    static void CrearFichero(){
-        Scanner entrada = new Scanner(System.in);
-        int numero=0;
-        try { 
-            FileOutputStream archivo = new FileOutputStream(ruta+"numeros.dat");
-            ObjectOutputStream salida=new ObjectOutputStream(archivo);
-            System.out.println("Teclea numeros 999 para finalizar");
-            while (numero!=999) {
-                numero=entrada.nextInt();
-                salida.writeInt(numero);
-            }
-            entrada.close();
-            salida.close();
-        }catch (IOException ioe) {
-            System.out.println("El fichero no se ha podido abrir");
-           
-        }
-    }
+static void listarFichero() {
+System.out.println("\n\tLISTAR FICHERO\n");
+int numero=0;
+try {
+ObjectInputStream lector=new ObjectInputStream(new FileInputStream(ruta+"numeros.dat"));
+System.out.println("\nLos números grabados en el fichero son: \n");
+numero=lector.readInt();
+while(numero!=999) {
+System.out.println(numero);
+numero=lector.readInt();
+}
+lector.close();
+}catch(IOException ioe) {System.out.println("El fichero no se puede abrir");}
+}
 
-    static void ListasFicheros(){
-        int num=0;
-        try {
-            FileInputStream entradaArchivo = new FileInputStream(ruta+"numeros.dat");
-            ObjectInputStream entrada = new ObjectInputStream(entradaArchivo);
-            while (num!=999) {
-                System.out.println(num);
-                num=entrada.readInt();
-            }
-            entrada.close();
-        } catch(IOException ioe){
-            System.out.println("El fichero no se ha podido abrir");
-        }
-    }
-    static boolean BuscarNumero(){
-        Scanner sc = new Scanner(System.in);
-        boolean encontrado = false;
-        int numero=0,numleido=0;
-        System.out.print("Teclea el numero que deseas buscar: ");
-        numero=sc.nextInt();
-        try {
-            FileInputStream entradaArchivo = new FileInputStream(ruta+"numeros.dat");
-            ObjectInputStream entrada = new ObjectInputStream(entradaArchivo);
-            System.out.println("Comienzo de busqueda...");
-            numleido=entrada.readInt();
-            while (numero!=999){
-                if (numleido==numero) {
-                    encontrado = true;
-                    break;
-                }
-                numleido=entrada.readInt();
-            }
-            System.out.println("Finalizacion de la busqueda");
-            entrada.close();
-            sc.close();
-        } catch (IOException e) {
-        }
-        return encontrado;
-    }
+static boolean buscarNumero( Scanner sc) {
+System.out.println("\n\tBUSCAR NÚMERO\n");
+int numeroBuscado=0, numero=0;
+boolean encontrado=false;
+System.out.print("Teclee el número a buscar: ");
+numeroBuscado=sc.nextInt();
 
-    static void BuscarNumeroRepetido(){
-        int numero=0,numleido=0,cont=0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Escribe el nuemro que deseas buscar cuantas beces se repite: ");
-        numero=sc.nextInt();
-        try {
-            FileInputStream entradaArchivo = new FileInputStream(ruta+"numeros.dat");
-            ObjectInputStream entrada = new ObjectInputStream(entradaArchivo);
-            System.out.println("Comienzo de busqueda...");
-            numleido=entrada.readInt();
-            while (numero!=999){
-                if (numleido==numero) {
-                    cont++;
-                }
-                numleido=entrada.readInt();
-            }
-            System.out.println("Finalizacion de la busqueda");
-            entrada.close();
-            sc.close();
 
-        } catch (IOException e) {
-        }
-        if (cont!=0) {
-            System.out.println("El nuemro esta grabado "+cont+" veces");
-        } else {
-            System.out.println("El nuemro no esta grabado ");
-        }
-    }
+try {
+ObjectInputStream lector=new ObjectInputStream(new FileInputStream(ruta+"numeros.dat"));
+numero=lector.readInt();
+while(numero!=999) {
+if(numero==numeroBuscado) {
+encontrado=true;
+numero=lector.readInt();
+break;
+}else {
+numero=lector.readInt();
+}
+}
 
-    public static void main(String[] args) {
-        int opcion =0;
-        opcion=menu();
-        switch(opcion){
-            case 1:
-                CrearFichero();
-                break;
-            case 2:
-                ListasFicheros();
-                break;
-            case 3: 
-                if(BuscarNumero()){
-                    System.out.println("El numero si que esta grabado");
-                }else{
-                    System.out.println("El numero no esta grabado");
-                }
-                break;
-            case 4:
-                BuscarNumeroRepetido();
-                break;
+}catch(IOException ioe) {System.out.println("El fichero no se puede abrir");}
+return encontrado;
+}
+static String buscarNumeroYContar(Scanner sc) {
+System.out.println("\n\tBUSCAR NUMERO Y CONTAR\n");
+int numeroBuscado=0, repeticiones=0, numero=0;
+String respuesta="";
+System.out.print("Teclee el número a buscar: ");
+numeroBuscado=sc.nextInt();
+try {
+ObjectInputStream lector=new ObjectInputStream(new FileInputStream(ruta+"numeros.dat"));
+numero=lector.readInt();
+while(numero!=999) {
+if(numero==numeroBuscado) {
+repeticiones++;
+numero=lector.readInt();
+}else {
+numero=lector.readInt();
+}
+}
 
-            default:
-                System.out.println("No has tecleado ninguna de las opciones");
-        }
-        
-    }
+}catch(IOException ioe) {System.out.println("El fichero no se puede abrir");}
+if(repeticiones==0) {
+respuesta="El número buscado no está grabado";
+}else{
+respuesta="El número buscado aparece "+repeticiones+" veces";
+}
+return respuesta;
+}
+
+
+
+static void fin() {
+System.out.println("\n\tFin del programa.");
+}
+
+public static void main(String[]args) {
+Scanner sc=new Scanner(System.in);
+int opcionElegida=0;
+
+do {
+opcionElegida=menu(sc);
+switch(opcionElegida) {
+case 1:
+crearFichero();
+break;
+case 2:
+listarFichero();
+break;
+case 3:
+if (!buscarNumero(sc)) {
+System.out.println("El número buscado NO está grabado");
+}else {
+System.out.println("El número buscado está grabado");
+}
+break;
+case 4:
+System.out.println(buscarNumeroYContar(sc));
+break;
+case 5:
+fin();
+break;
+}
+
+
+}while (opcionElegida!=5);
+}
 }
